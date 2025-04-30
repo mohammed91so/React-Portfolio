fc8c7cf392e7cf41490dd29892bf8829dd7fb957
mohammed91so <150184232+mohammed91so@users.noreply.github.com>
2025-04-30 12:58:58 +0100

    links

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,10 +22,10 @@ export default function Hero() {
             </p>
 
             <div className="social-links">
-              <a href="https://github.com/mohammed91so" className="social-icon">
+              <a href="https://github.com/mohammed91so" target='_blanck' className="social-icon">
                 <Github size={20} />
               </a>
-              <a href="https://www.linkedin.com/in/mohammed-el-harbouli-04850b305?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="social-icon">
+              <a href="https://www.linkedin.com/in/mohammed-el-harbouli-04850b305?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target='_blanck' className="social-icon">
                 <Linkedin size={20} />
               </a>
               <a href="#" className="social-icon">
